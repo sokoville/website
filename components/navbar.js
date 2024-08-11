0c2ddb38d4b1fdcb8993092fadac377759038bca
sokoville <59071056+sokoville@users.noreply.github.com>
2024-08-11 10:41:10 -0500

    PLEASE USE THE STYLE

--- components/navbar.js
+++ components/navbar.js
@@ -4,6 +4,8 @@ class NavBar extends HTMLElement {
 	}
   
 	connectedCallback() { this.innerHTML = `
+		<link rel="stylesheet" href="styles.css">
+
 		<div id="navbar">
 			<a href="index.html" title="go back home"><img src="images/ui/huis.gif" id="navbar_home"></a><br>
 			<a href="linkzone.html" title="HYPERLINKS"><img src="images/ui/chainlink.png" id="navbar_links"></a>
