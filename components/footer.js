23b1a3cf5c2a24be283c15898ef8b9112e7965bd
sokoville <59071056+sokoville@users.noreply.github.com>
2024-08-12 00:37:57 -0500

    improvements to layout

--- components/footer.js
+++ components/footer.js
@@ -11,8 +11,8 @@ class Footer extends HTMLElement {
 			<a href="https://www.twitter.com/kafkaesquetwink" title="twitter is a terrible place but a place i inhabit"><img src="badges/twitter.gif" alt="twitter"></a>
 			<a href="https://discord.gg/jARfRWxm" title="ugh"><img src="badges/discord.gif" alt="discord server"></a>
 			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="come play counter strike with me"><img src="badges/playwithsteam.jpg" alt="profile steam"></a>
-			<a href="" title="optimized for no one"><img src="badges/anybrowse.gif"></a>
-			<a href="" title="seriously fuck nfts"><img src="badges/antinft.gif" alt="fuck nfts"></a>
+			<img src="badges/anybrowse.gif" title="optimized for no one">
+			<img src="badges/antinft.gif" alt="fuck nfts" title="seriously fuck nfts">
 			<a href="https://www.mozilla.org/nl/firefox/" title="mozilla has a lot of problems, but at least they aren't google"><img src="badges/anythingbut.gif" alt="anything but chrome"></a>
 			<a href="https://www.debian.org/index.nl.html" title="linux is good, i like linux"><img src="badges/poweredbydeb.gif" alt="powered but debian linux"></a>
 			<img src="badges/cogs.gif">
