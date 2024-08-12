445524a8eb8fdb165fbb265adf8323ac03f60886
sokoville <59071056+sokoville@users.noreply.github.com>
2024-08-12 12:52:50 -0500

    no more reset

--- components/comments.js
+++ components/comments.js
@@ -101,11 +101,11 @@ function getComments() {
 		if (comments.length == 0 || Object.keys(comments[0]).length < 2) { // Once again, Google Sheets can be weird
 			c_container.innerHTML = s_noCommentsText;
 		} else {
-			let child = document.getElementById("comments_list").lastElementChild;
+			/*let child = document.getElementById("comments_list").lastElementChild;
        		while (child) {
             	document.getElementById("comments_list").removeChild(child);
             	child = document.getElementById("comments_list").lastElementChild;
-        	}
+        	}*/
 
 			for (i = 0; i < comments.length; i++) {
 				createComment(comments[i]["Timestamp2"], comments[i]["Text"], comments[i]["Name"])
