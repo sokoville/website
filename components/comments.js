fe124e5816ea09703c74ae239c17e1bb6fba62c7
sokoville <59071056+sokoville@users.noreply.github.com>
2024-08-11 21:55:35 -0500

    STRING

--- components/comments.js
+++ components/comments.js
@@ -89,7 +89,7 @@ function getComments() {
 				comment_line.appendChild(span1)
 
 				if (!comments[i]["Name"] == "") {
-					comment_line.appendChild(document.createTextNode(concat(' (', comments[i]['Name'], ') ')))
+					comment_line.appendChild(document.createTextNode(String.concat(' (', comments[i]['Name'], ') ')))
 				} else {
 					comment_line.appendChild(document.createTextNode(" (anon) "))
 				}
