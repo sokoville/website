409be0a04861ae1ac3ee8eb895a71654e3871206
sokoville <59071056+sokoville@users.noreply.github.com>
2024-08-20 19:45:24 -0500

    PAGE

--- components/comments.js
+++ components/comments.js
@@ -101,7 +101,7 @@ function getComments(postSubmit) {
 					comment[json.table.cols[c].label] = val2;
 				}
 				comment.Timestamp2 = json.table.rows[r].c[0].f;
-				comment.Page = json.table.rows[r].c[3].f;
+				console.log(comment["Page"]);
 				
 				if (comment["Page"] == location.pathname) {
 					comments.push(comment);
