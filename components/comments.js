9c5071782c84c8246f6b4a45732ef411cd7dea19
sokoville <59071056+sokoville@users.noreply.github.com>
2024-11-02 00:25:37 -0500

    Update comments.js

--- components/comments.js
+++ components/comments.js
@@ -27,6 +27,10 @@ class Comments extends HTMLElement {
 			<div id="comments_list" style="text-align: left;"></div>
 			<iframe id="hiddenIFrame" name="hiddenIFrame" style="display: none;"></iframe>
 		</div>
+
+		<script>
+			document.getElementsByClassName("page_input").value = document.location.pathname
+		</script>
 	`;}
 }
 
@@ -127,8 +131,6 @@ function getComments(postSubmit) {
 			document.getElementById("comment").reset();
 		}
 	})
-
-	document.getElementsByClassName("page_input").value = document.location.pathname
 }
 
 
