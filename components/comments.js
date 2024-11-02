7fefbc8c8cce337a5ee616602a888f3f433df24c
sokoville <59071056+sokoville@users.noreply.github.com>
2024-11-02 01:02:44 -0500

    f

--- components/comments.js
+++ components/comments.js
@@ -16,7 +16,7 @@ class Comments extends HTMLElement {
 				<div>
 					Comment:
 					<input id="comment_text" type="text" name="entry.1007718193" maxlength="140" minlength="3" value="" style="width: 40%" required>
-					<input id="page_input" type="text" name="entry.1374517791" value="/" style="display: none;">
+					<input id="page_input" type="text" name="entry.1374517791" style="display: none;">
 					<input type="submit" value="Comment"><br>
 					<span style="font-size: .75em">(prefer English or Nederlands language)</span>
 				</div>
