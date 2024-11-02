f6d1bf914d9585a9ee2ec4d9d9746fcc2dad088f
sokoville <59071056+sokoville@users.noreply.github.com>
2024-11-02 01:00:32 -0500

    df

--- components/comments.js
+++ components/comments.js
@@ -25,12 +25,6 @@ class Comments extends HTMLElement {
 			<div id="comments_list" style="text-align: left;"></div>
 			<iframe id="hiddenIFrame" name="hiddenIFrame" style="display: none;"></iframe>
 		</div>
-
-		<script>
-			document.getElementById("page_input").value = document.location.pathname;
-			console.log(document.getElementById("page_input").value);
-			console.log(document.location.pathname);
-		</script>
 	`;}
 }
 
