a8acb8043a1504eebf08fe5e010a97361460083f
sokoville <59071056+sokoville@users.noreply.github.com>
2025-05-16 08:22:04 -0500

    slight improvements

--- components/footer.js
+++ components/footer.js
@@ -7,14 +7,14 @@ class Footer extends HTMLElement {
 		<footer>
 			<a href="https://letterboxd.com/iminthecut/">letterboxd</a> | 
 			<a href="https://www.nytimes.com/subscription/redeem?campaignId=8WH8J&gift_code=0412a4eca912f48f">free nyt trial</a>
-			<br><small>last updated: 15.05.2025 (ver3)</small><br>
-			<a href="https://www.twitter.com/whenitstops" title="twitter is a terrible realm i inhabit"><img src="badges/twitter.gif" alt="twitter"></a>
-			<!--<a href="https://discord.gg/" title="ugh"><img src="badges/discord.gif" alt="discord server"></a>-->
+			<br><small>last updated: 16.05.2025 (ver3)</small><br>
+			<a href="https://www.twitter.com/whenitstops"><img src="badges/twitter.gif" alt="twitter"></a>
+			<!--<a href="https://discord.gg/"><img src="badges/discord.gif" alt="discord server"></a>-->
 			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="play counter strike with me"><img src="badges/playwithsteam.jpg" alt="profile steam"></a>
-			<img src="badges/anybrowse.gif" title="optimized for nothing">
-			<img src="badges/antinft.gif" alt="fuck nfts" title="ew">
-			<a href="https://www.mozilla.org/nl/firefox/" title="mozilla firefox"><img src="badges/anythingbut.gif" alt="anything but chrome"></a>
-			<a href="https://www.debian.org/index.nl.html" title="linux is good, i like linux"><img src="badges/poweredbydeb.gif" alt="powered but debian linux"></a>
+			<img src="badges/anybrowse.gif">
+			<img src="badges/antinft.gif">
+			<a href="https://www.mozilla.org/nl/firefox/"><img src="badges/anythingbut.gif"></a>
+			<a href="https://www.debian.org/index.nl.html"><img src="badges/poweredbydeb.gif" alt="powered but debian linux"></a>
 			<img src="badges/cogs.gif">
 			<a href="https://youtu.be/EDsc5A1LQMs?si=TyDRH-74LRuM67d_" title="counter strike source: a beautiful creation"><img src="badges/cs.gif" alt="counter strike source"></a>
 			<img src="badges/fingerofgod.gif">
