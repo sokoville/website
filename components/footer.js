18dbd34d2d81d476e7483cd887040e9b1a2009d8
sokoville <59071056+sokoville@users.noreply.github.com>
2025-12-04 13:33:00 -0600

    a

--- components/footer.js
+++ components/footer.js
@@ -6,18 +6,18 @@ class Footer extends HTMLElement {
 	connectedCallback() { this.innerHTML = `
 		<footer>
 			<br><small>last updated: 16.05.2025 (ver3)</small><br>
-			<a href="https://www.twitter.com/whenitstops"><img src="badges/twitter.gif" alt="twitter"></a>
-			<!--<a href="https://discord.gg/"><img src="badges/discord.gif" alt="discord server"></a>-->
-			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="play counter strike with me"><img src="badges/playwithsteam.jpg" alt="profile steam"></a>
-			<img src="badges/anybrowse.gif">
-			<img src="badges/antinft.gif">
-			<a href="https://www.mozilla.org/nl/firefox/"><img src="badges/anythingbut.gif"></a>
-			<a href="https://www.debian.org/index.nl.html"><img src="badges/poweredbydeb.gif" alt="powered but debian linux"></a>
-			<img src="badges/cogs.gif">
-			<a href="https://youtu.be/EDsc5A1LQMs?si=TyDRH-74LRuM67d_" title="counter strike source: a beautiful creation"><img src="badges/cs.gif" alt="counter strike source"></a>
-			<img src="badges/fingerofgod.gif">
-			<a href="https://www.youtube.com/watch?v=S5L3tESenlU" title="põgene vaba laps"><img src="badges/howtobecomeanangel.gif"></a>
-			<img src="badges/mousepow.gif">
+			<a href="https://www.twitter.com/whenitstops"><img src="https://deadworld.de/badges/twitter.gif" alt="twitter"></a>
+			<!--<a href="https://discord.gg/"><img src="https://deadworld.de/badges/discord.gif" alt="discord server"></a>-->
+			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="play counter strike with me"><img src="https://deadworld.de/badges/playwithsteam.jpg" alt="profile steam"></a>
+			<img src="https://deadworld.de/badges/anybrowse.gif">
+			<img src="https://deadworld.de/badges/antinft.gif">
+			<a href="https://www.mozilla.org/nl/firefox/"><img src="https://deadworld.de/badges/anythingbut.gif"></a>
+			<a href="https://www.debian.org/index.nl.html"><img src="https://deadworld.de/badges/poweredbydeb.gif" alt="powered but debian linux"></a>
+			<img src="https://deadworld.de/badges/cogs.gif">
+			<a href="https://youtu.be/EDsc5A1LQMs?si=TyDRH-74LRuM67d_" title="counter strike source: a beautiful creation"><img src="https://deadworld.de/badges/cs.gif" alt="counter strike source"></a>
+			<img src="https://deadworld.de/badges/fingerofgod.gif">
+			<a href="https://www.youtube.com/watch?v=S5L3tESenlU" title="põgene vaba laps"><img src="https://deadworld.de/badges/howtobecomeanangel.gif"></a>
+			<img src="https://deadworld.de/badges/mousepow.gif">
 			<br>
 		</footer>
 	`;}
