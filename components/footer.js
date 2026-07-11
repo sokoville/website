a6b9e2031774cdb4f83159a73a3e9adccf58cc17
sokoville <59071056+sokoville@users.noreply.github.com>
2026-07-11 17:39:48 -0400

    new matthew

--- components/footer.js
+++ components/footer.js
@@ -6,13 +6,12 @@ class Footer extends HTMLElement {
 	connectedCallback() { this.innerHTML = `
 		<footer>
 			<br><small>last updated: 16.05.2025 (ver3)</small><br>
-			<a href="https://www.twitter.com/whenitstops"><img src="https://deadworld.de/badges/twitter.gif" alt="twitter"></a>
+			<a href="https://www.twitter.com/"><img src="https://deadworld.de/badges/twitter.gif" alt="twitter"></a>
 			<!--<a href="https://discord.gg/"><img src="https://deadworld.de/badges/discord.gif" alt="discord server"></a>-->
 			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="play counter strike with me"><img src="https://deadworld.de/badges/playwithsteam.jpg" alt="profile steam"></a>
 			<img src="https://deadworld.de/badges/anybrowse.gif">
 			<img src="https://deadworld.de/badges/antinft.gif">
 			<a href="https://www.mozilla.org/nl/firefox/"><img src="https://deadworld.de/badges/anythingbut.gif"></a>
-			<a href="https://www.debian.org/index.nl.html"><img src="https://deadworld.de/badges/poweredbydeb.gif" alt="powered but debian linux"></a>
 			<img src="https://deadworld.de/badges/cogs.gif">
 			<a href="https://youtu.be/EDsc5A1LQMs?si=TyDRH-74LRuM67d_" title="counter strike source: a beautiful creation"><img src="https://deadworld.de/badges/cs.gif" alt="counter strike source"></a>
 			<img src="https://deadworld.de/badges/fingerofgod.gif">
@@ -23,4 +22,4 @@ class Footer extends HTMLElement {
 	`;}
 }
 
-customElements.define('footer-component', Footer);
+customElements.define('footer-comp', Footer);
