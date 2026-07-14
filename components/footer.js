a727ffcd8ffd44e85fe33ce678c5065e71ef0ba0
sokoville <59071056+sokoville@users.noreply.github.com>
2026-07-14 15:23:11 -0400

    spotify test

--- components/footer.js
+++ components/footer.js
@@ -10,11 +10,10 @@ class Footer extends HTMLElement {
             <span style="font-size: 24px;"><a style="text-decoration: none;" href="https://deadworld.de/">舎</a> </span>
             </div>-->
 			<a href="https://www.twitter.com/"><img src="https://deadworld.de/badges/twitter.gif" alt="twitter"></a>
+            <a href="https://letterboxd.com/iminthecut/" title="letterboxd"><img src="https://deadworld.de/badges/letterboxd.png" alt="letterboxd"></a>
 			<!--<a href="https://discord.gg/"><img src="https://deadworld.de/badges/discord.gif" alt="discord server"></a>-->
 			<a href="https://steamcommunity.com/profiles/76561198852125837/" title="play counter strike with me"><img src="https://deadworld.de/badges/playwithsteam.jpg" alt="steam"></a>
-            <a href="https://letterboxd.com/iminthecut/" title="letterboxd"><img src="https://deadworld.de/badges/letterboxd.png" alt="letterboxd"></a>
 			<img src="https://deadworld.de/badges/anybrowse.gif" alt="">
-			<img src="https://deadworld.de/badges/antinft.gif" alt="">
 			<a href="https://www.mozilla.org/nl/firefox/"><img src="https://deadworld.de/badges/anythingbut.gif" alt=""></a>
 			<img src="https://deadworld.de/badges/cogs.gif" alt="">
 			<a href="https://youtu.be/EDsc5A1LQMs?si=TyDRH-74LRuM67d_" title="counter strike source: a beautiful creation"><img src="https://deadworld.de/badges/cs.gif" alt="counter strike source"></a>
---
+++ components/spotify.js
@@ -0,0 +1,104 @@
+const WORKER_URL = "https://last-fm-proxy.matthewbogen06.workers.dev";
+
+let params = new URLSearchParams(window.location.search);
+let playing = false;
+let imgcache = new Image();
+
+function requestUpdate(user) {
+    let url = `${WORKER_URL}/?user=${encodeURIComponent(user)}`;
+    let request = new Request(url, {
+        "method": "GET",
+    });
+    return fetch(request);
+}
+
+function successHandler(value) {
+    if (value.status !== 200) {
+        return;
+    }
+    value.json().then(data => {
+        //console.log(data);
+        let track = data.recenttracks.track[0];
+        let img = track.image[0]["#text"];
+        for (let imgdata of track.image) {
+            img = imgdata["#text"];
+        }
+        if (img == "") {
+            let art = track.artist;
+            img = art.image[0]["#text"];
+            for (let imgdata of art.image) {
+                img = imgdata["#text"];
+            }
+        }
+        if (img == "") {
+            // Seems to be the default "no image" logo.
+            img = "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png";
+        }
+        imgcache.src = img;
+        let el_info = document.querySelector(".info");
+        let el_logo = document.querySelector(".info .logo");
+        let el_artist = document.querySelector(".info .artist");
+        let el_title = document.querySelector(".info .title");
+        if (track["@attr"] == undefined) {
+            if (playing) {
+                // Stop Playing
+                console.log("Stopped playing track.");
+                playing = false;
+            }
+        } else if (playing == false) {
+            // Start Playing
+            console.log("Started playing track.");
+            playing = true;
+            document.getElementById("musicdisplay").style.display = "flex";
+            // document.getElementById("bruh").style.display = "block";
+            imgcache.src = img;
+            el_logo.src = img;
+            el_artist.innerText = track.artist.name;
+            el_title.innerText = track.name;
+
+        } else {
+            // Track Change
+            let old_artist = el_artist.innerText;
+            let old_title = el_title.innerText;
+            if ((old_artist != track.artist.name) || (old_title != track.name)) {
+                console.log("Changed track.");
+                flip(function() {
+                    el_logo.src = img;
+                    el_artist.innerText = track.artist.name;
+                    el_title.innerText = track.name;
+                });
+                document.getElementById("musicdisplay").style.display = "flex";
+                // document.getElementById("bruh").style.display = "block ";
+            }
+        }
+    }, reason => {
+    })
+
+    setTimeout(tick, 15000);
+}
+
+function failureHandler(reason) {
+    console.log("Last.FM Query failed:", reason)
+    setTimeout(tick, 60000);
+}
+
+function tick() {
+    let rq = requestUpdate("sokocian");
+    rq.then(successHandler, failureHandler);
+    rq.catch(failureHandler);
+}
+
+function zoom() {
+    let width = document.documentElement.clientWidth;
+    let height = document.documentElement.clientHeight;
+    let csize = document.documentElement.getBoundingClientRect();
+    let wscale = width / csize.width;
+    let hscale = height / csize.height;
+    document.body.style.zoom = hscale;
+}
+
+(function() {
+    console.log(window.location.origin);
+    zoom();
+    tick();
+})();
